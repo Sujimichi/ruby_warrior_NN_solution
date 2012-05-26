71d479b0a47eb2158968dae946496b6a0f2961da
sujimichi <sujimichi@googlemail.com>
2012-05-26 03:35:36 +0100

    added autosave to BasicTraining, comments and stuff

--- bootcamp.rb
+++ bootcamp.rb
@@ -1,6 +1,51 @@
 require "./brains.rb"
 require "./darwin.rb"
 
+#This file contains a collection of classes which are used in the evolution of the neural networks.  Nothing in here is needed to run a trained NN in rubywarrior.
+#There are several classes which are used to run evolution on 'populations' in a number of ways.
+#
+#BootCamp - used to evolve a population of NN's to respond to a set of predefined inputs and outputs 
+#The predefined inputs/outputs are from AssaultCourse and DrillSergeant observes a NN's performance over the AssaultCourse
+#
+#CombatTraining - used to evolve NN's in a rubywarrior level
+#The performance of the NN in the rubywarrior level is determined by Invigilator
+#
+#FieldTraining - used to evolve NN's over several/all rubywarrior levels.
+#requires some setup and levels to be unlocked; A specific rubywarrior folder needs to be created for each level according to a naming convention see FieldTraining for more.
+#
+#AgentTraining - used to evolve NN's in rubywarrior EPIC mode.
+#requires epic unlocked.
+#
+#Field and Agent use Threads to evaluate a NN's performance in all levels together, thier output gets a bit nutts at times ;)
+#
+#All four training grounds inherit from BasicTraining 
+#Provides some common methods for runnning evolution, saving/loading/autosaving populations to file and some getting/setter methods for the GA (population,muation_rate)
+#
+#Each of the four 'training grounds' set the args for a genetic alg and define the 'fitness function'.  All follow the same basic format and only define an initialize method, ie;
+#
+# class BootCamp < BasicTraining
+#   def initialize n_layers
+#     #init vars
+#     @ga = MGA.new(:generations => 1000, :mutation_rate => 10, :gene_length => @gene_length, :fitness => Proc.new{|genome, gen|
+#       nn = Brains[n_layers-1].new(@nodes, genome)
+#       #code to test nn's performance and return its score 
+#     })
+#   end
+# end
+#
+#Training grounds take a single integer arg in thier initialize method which defines how many layers (n_layers) the NN should have; 1, 2 or 3.
+#The :set_config_for method in BasicTraining defines how many nodes to used in each layer as sets @nodes.
+#
+#Brains (from brains.rb) simply indexes the three types of NN, 1, 2 and 3 layer, ie; Brians[0] returns a 1 layer NN Brain.  see brains.rb for more.
+#
+#Usage
+#
+#bc = BootCamp.new(2)
+#bc.train
+#bc.save_pop "popname"
+#bc.write_best #find best genome and save to genome file
+#
+
 #BOOT CAMP - basic training
 #subject neural nets to a pre-defined set of inputs and award points for correct actions
 #Boot camp can be run very quickly for several thousand generations to get the population closer to required behavior
@@ -10,14 +55,14 @@ require "./darwin.rb"
 #selection/mutation events need to happen before any points are awarded atall.  
 
 
-#base class which BootCamp and CombatTraining inherit.  Simply provides methods for initializing a 1,2 or 3 layer NN(Brain), 
-#saving and loading the population to file and running the GA.
+#base class which all training grounds inherit.
 class BasicTraining
   attr_accessor :ga, :nodes, :gene_length
 
-  def set_config_for n_layers
+  def set_config_for n_layers = 2
+    raise "Look, just no!" unless [1,2,3].include?(n_layers)
     @warrior_name = Dir.getwd.split("/").last.sub("-beginner", "")
-    @n_layers = n_layers
+    @n_layers = n_layers   
     nodes_1_layer = {:in => 16, :out => 8}
     nodes_2_layer = {:in => 16, :inner => 6, :out => 8}
     nodes_3_layer = {:in => 16, :inner => 6, :inner2 => 6, :out => 8}
@@ -25,8 +70,39 @@ class BasicTraining
     @gene_length = Brain.required_genome_length_for(nodes)
   end
 
-  def train
-    @ga.evolve
+  #calls evolve on the Genetic Algorithm in @ga.  also provides functionality for auto saving the population during evolution.
+  def train use_new_name = false
+    if @auto_save_every_n_generations #can be set to an int to have the population written to file even n generations.  
+      n = @auto_save_every_n_generations
+      i = @ga.generations/n
+      #workout what name to use to auto save population as.  If use_name_name is true a new name will be used for that call to :train.  
+      # ie 
+      # bootcamp.train #=> first time sets a new name based on inital state
+      # bootcamp.train #=> stop execution and then run again, will use same name as before
+      # bootcamp.train true #=> will use a new name based on current state
+      # bootcamp.train "custom_name" #=> will use the given name.
+      if use_new_name && !use_new_name.eql?(true) #not false, but not true; a string perhaps
+        @uniq_name = use_new_name 
+        use_new_name = false
+      end
+      use_new_name = true unless @uniq_name
+      if use_new_name
+        require 'digest'
+        d = Digest::MD5.new
+        d << @ga.instance_variables.map{|v| @ga.instance_variable_get(v) }.compact.join      
+        @uniq_name = d.hexdigest 
+      end
+      name = "current_pop_#{@nodes.size-1}layer-#{@nodes.values.join("-")}_#{self.class.to_s}_#{@uniq_name}"     
+
+      i.times do |i|
+        @ga.evolve(n)
+        print "\n\nran #{n} generations.  Saving population as #{name}...."
+        self.save_pop name
+        puts "done.\n\n"
+      end
+    else
+      @ga.evolve
+    end  
   end
   alias run train
 
@@ -42,10 +118,9 @@ class BasicTraining
     @ga.population = new_population
   end
 
-  def save_pop
-    PopBuilder.save_pop(@ga.population)
+  def save_pop name = nil
+    PopBuilder.save_pop(@ga.population, name)
   end
-
   def load_pop f_name
     pop = PopBuilder.load_pop(f_name)
     @ga.population = pop
@@ -69,10 +144,11 @@ class BasicTraining
   end
 
   def remark_on score
+    @target_score ||= 0
     if score > @highest_score 
       @highest_score = score
       print "\t\t<----BestSoFar"
-    elsif score == @highest_score && score > 0
+    elsif score == @highest_score && score >= @target_score
       print "\t\t<----Combat Ready!!"
     end
   end
@@ -80,6 +156,17 @@ class BasicTraining
   def reset_high_score
     @highest_score = -10000
   end
+
+  def graduate
+    training_grounds = [BootCamp, CombatTraining, FieldTraining, AgentTraining]
+    cur_pos = training_grounds.index(self.class)
+    return "No more training grounds, run write_best and go kick RW's ass!" if cur_pos >= training_grounds.size-1
+    new_training_groud = training_grounds[cur_pos.next].new(@n_layers)
+    new_training_groud.population = self.population.clone
+    new_training_groud
+  end
+
+  
 end
 
 #BootCamp runs the evolution of a population of NNs over training examples from AssaultCourse.
@@ -88,9 +175,10 @@ class BootCamp < BasicTraining
 
   #n_layers - define which NN to use 1, 2 or 3 layered.
   def initialize n_layers = 2 
+    @auto_save_every_n_generations = 1000
     set_config_for n_layers
 
-    @ga = MGA.new(:generations => 100000, :mutation_rate => 10, :gene_length => @gene_length, :popsize => 80, :fitness => Proc.new{|genome, gen|
+    @ga = MGA.new(:generations => 100000, :mutation_rate => 10, :gene_length => @gene_length, :popsize => 30, :fitness => Proc.new{|genome, gen|
       print "#{gen} |"
 
       @recruit = Brains[@n_layers-1].new(@nodes, genome)  #initialize brain(neural net) with current genome.
@@ -118,18 +206,14 @@ class BootCamp < BasicTraining
     }) 
   end
 
-  def graduate
-    ct = CombatTraining.new(@n_layers)
-    ct.ga.population = @ga.population
-    ct
-  end
 
 end
 
 #CombatTraining runs the evolution of a population of NNs in the current level of rubywarrior.
 class CombatTraining < BasicTraining
 
-  def initialize n_layers
+  def initialize n_layers = 2
+    @auto_save_every_n_generations = 500
     set_config_for n_layers
     reset_high_score
 
@@ -157,8 +241,10 @@ end
 #Requires some setup.  Needs a rubywarrior dir setup for each level named levelxbot where x is the level number.
 class FieldTraining < BasicTraining
 
-  def initialize n_layers
-    set_config_for n_layers
+  def initialize n_layers =2
+    @auto_save_every_n_generations = 100
+    @target_score = 842
+    set_config_for n_layers   
     reset_high_score
 
     @initial_dir = Dir.getwd
@@ -169,6 +255,10 @@ class FieldTraining < BasicTraining
     @ga =MGA.new(:generations => 5000, :mutation_rate => 2, :gene_length => @gene_length, :fitness => Proc.new{|genome, gen|
       puts "#{gen}\n"
       Dir.chdir(rootdir)
+
+      level_factor = [0.8, 1.0, 0.8, 0.6, 0.4, 0.9, 1.0, 1.0, 1.0]
+
+      puts "\n\n"
     
       threads = []
       levels.each do |lvl|
@@ -180,12 +270,16 @@ class FieldTraining < BasicTraining
           invigilator = Invigilator.new #invigilator class examins output from rubywarrior and assigns points for various actions.  Invigilator#score_results == the fitness function
           results = `rubywarrior -t 0 -s` #run runywarrior
           #use invigilator to get the final score.  Also returns the break down of points for displaying.
-          score, level_score, level_total, n_turns, turn_score, time_bonus, clear_bonus = invigilator.score_results(results)
+          score, level_score, level_total, n_turns, turn_score, time_bonus, clear_bonus = invigilator.score_results(results)   
+
+          score = score * level_factor[lvl-1]
+
+
           puts "level-#{lvl}|levelscore: #{level_score} | turnscore: #{turn_score.round(2)} | bonus(t:c): #{time_bonus}:#{clear_bonus} | turns: #{n_turns} | Total: #{level_total} | fitnes: #{score.round(2)}"
           instance_variable_set("@ans#{lvl}", score) #set result in an @var ie @ans1.  Done so threads don't try to write answer to a common var.
           Dir.chdir(@initial_dir)
         }
-        sleep(0.5) #This sleep is a horrible hack arround the problem of current directory not being thread safe.
+        sleep(0.3) #This sleep is a horrible hack arround the problem of current directory not being thread safe.
         #For each level it first changes into the levels directory and then runs rubywarrior in a new Thread.  Then sleeps.  
         #After the sleep it moves the the next level and moves to its dir and again runs rubywarrior in a new Thread.  
         #Without the sleep all the threads would be started almost at the same time and would start in which ever directory was now the current directory.
@@ -211,7 +305,9 @@ end
 #AgentTrainingruns the evolution of a population of NNs over all the levels of rubywarrior in epic mode.
 #Only available once passed epic mode.  
 class AgentTraining < BasicTraining
-  def initialize n_layers
+  def initialize n_layers = 2
+    @auto_save_every_n_generations = 100
+    @target_score = 842
     set_config_for n_layers
     reset_high_score
 
@@ -400,9 +496,9 @@ end
 
 
 
-#DrillSergeant provides a way of testing a NN's response to a given input.  It has a method 'test_recruit_on' which takes a hash which defines {input_array => response_array}
+#DrillSergeant provides a way of testing a NN's response to a given input.  The method 'test_recruit_on' takes a hash which defines {input_array => response_array}
 #response_array must include [action, impulse] but can also include an alternative 'code' and different point value ie[:walk, :forward, 'wfd', 2]
-#Several examples can be passed in one test.  .score will return the current score which is an array of accumulated points
+#Several examples can be passed in one test.  1.score will return the current score which is an array of accumulated points
 #
 # d = DrillSergeant.new
 # d.recruit = recruit #recruit is a 'brain' from Brains
@@ -465,9 +561,11 @@ class PopBuilder
     JSON.parse pop.first
   end
 
-  def self.save_pop population
-    puts "enter a file name for population"
-    f_name = gets.chomp 
+  def self.save_pop population, f_name = nil
+    unless f_name
+      puts "enter a file name for population"
+      f_name = gets.chomp 
+    end
     return "no filename" if f_name.nil? || f_name.empty?
     pop_file = "./#{f_name}"
     File.open(pop_file,'w'){|f| f.write( population )}   
